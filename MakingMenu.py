3cee225f37610daea1f60ffadc1cf714ea374148
Brian Ezeanya <151948178+beze10@users.noreply.github.com>
2025-10-17 12:47:23 +0100

    Add files via upload
    Music Program file

---
+++ MakingMenu.py
@@ -0,0 +1,191 @@
+import os
+from pickle import TRUE
+import sys
+
+
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+# print("Welcome to the waveform generator program! Press enter to continue")
+# userInput = input("Select the number between 1 and for your choice of settings:")
+# cls()
+
+def select_waveform():
+    cls()
+    print("1) Sine wave")
+    print("2) Square wave")
+    print("3) Triangle wave")
+    print("4) Sawtooth wave")
+
+    inputText = input("Please select a number between 1 and 4: ")
+    match inputText:
+        case '1':
+            cls()
+            print("You selected Sine wave")
+            input()
+        case '2':
+            cls()
+            print("You selected Square wave")
+            input()
+        case '3':
+            cls()
+            print("You selected Triangle wave")
+            input()
+        case '4':
+            cls()
+            print("You selected Sawtooth wave")
+            input()            
+        case _:
+            cls()
+            print("The input value is not valid. Please try again.")
+            input()
+            
+current_loudness = 50
+
+def loudness():                  #Ai helped me with using global variable and understanding how to implement try and except block
+    cls()
+    global current_loudness    
+    while True:
+        userInput = input("Set loudness level between 0 and 100 :")
+        if userInput == '':
+            cls()
+            print("Input unchanged. Press enter to return to main menu")
+            return
+        
+        try:
+            value = int(userInput)
+        except ValueError:
+            cls()
+            print("Invalid input. Please enter a number between 0 and 100.")
+            input()
+        
+        if 0 <= value <=100:
+            current_loudness = value
+            cls()
+            print(f"Loudness set to {current_loudness}")
+            input()
+            break
+        else:
+            cls()
+            print("The value must be between 0 and 100. Please try again")
+            input("Press enter to try again.")
+            
+
+def ABC_file_path():
+    cls()
+
+
+                
+
+if __name__ == "__main__":
+    while(TRUE):
+        cls()
+        print("1) Select waveform type")
+        print("2) Setting the loudness")
+        print("3) Indicating ABC file path")
+        print("4) Changing speed (BPM)")
+        print("5) Shift Pitch")
+        print("6) Add Background Noise")
+        print("7) Mixing within extenral WAV file")
+        print("8) Play file")
+        print("9) Save music as WAV file")
+        print("10) Exit program")
+        inputText = input("Please select a number between 1 and 10: ")
+        match inputText:
+            case '1':
+                select_waveform()
+            case '2':
+                loudness()
+            # case '3':
+            #     option3()
+            # case '4':
+            #     option4()
+            # case '5':
+            #     option5()            
+            # case '6':
+            #     option6()            
+            case _:
+                cls()
+                print("The input value is not valid. Please try again.")
+                input()
+
+# if userInput == '1':
+#     print("You selected 1 for Selecting waveform type")
+#     input()
+#     select_waveform()
+# elif userInput == '2':
+#     print("You selected 2 for Loudness serttings")
+#     input()
+#     loudness()
+    
+# def option1():
+#     cls()
+#     print("You selected the first option")
+#     select_waveform()
+
+
+
+
+# def select_waveform():
+#     cls()
+#     print("Select your type of waveform")
+#     input()
+#     print("1) Sine wave")
+#     print("2) Square wave")
+#     print("3) Triangle wave")
+#     print("4) Sawtooth wave")
+#     inputText = input("Please select a number between 1 and 4: ")
+#     match inputText:
+#         case '1':
+#             cls()
+#             print("You selected Sine wave")
+#             input()
+#         case '2':
+#             cls()
+#             print("You selected Square wave")
+#             input()
+#         case '3':
+#             cls()
+#             print("You selected Triangle wave")
+#             input()
+#         case '4':
+#             cls()
+#             print("You selected Sawtooth wave")
+#             input()            
+#         case _:
+#             cls()
+#             print("The input value is not valid. Please try again.")
+#             input()
+    
+
+
+
+# def option2():
+#     cls()
+#     print("You selected the second option");
+#     input()
+
+# def option3():
+#     cls()
+#     print("You selected the third option");
+#     input()
+
+# def option4():
+#     cls()
+#     print("You selected the fourth option");
+#     input()
+
+# def option5():
+#     cls()
+#     print("You selected the fifth option");
+#     input()
+
+# def option6():
+#     cls()    
+#     yesNo = input("Are you sure you want to exit the program?(y=yes/n=no)")
+#     if yesNo=='y':
+#         sys.exit()
+
+
+
